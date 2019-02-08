e531eae908c5aa3350fba381a1f0ad3537e41663
Katarina Svorcan <kata_che@hotmail.com>
2019-02-07 17:54:24 -0800

    YouTube API and DB seed complete

---
+++ config/youtube.config.example.js
@@ -0,0 +1,4 @@
+// Put your YouTube API keys here!
+var YOUTUBE_API_KEY = 'YOUR KEY SHOULD GO HERE';
+
+module.exports = YOUTUBE_API_KEY;
--- db/VideoModel.js
+++ db/VideoModel.js
@@ -4,12 +4,13 @@ const videoSchema = new mongoose.Schema({
   numberOfDislikes: { type: Number, default: 0 },
   numberOfLikes: { type: Number, default: 0 },
   title: String,
+  videoYouTubeId: String,
   videoOwnerId: String,
   videoOwnerUsername: String,
   videoThumbnailURL: String,
   videoURL: String,
-  viewCount: { type: Number, default: 0 }
-});
+  viewCount: { type: Number, default: 0 },
+}, { timestamps: { createdAt: 'created_at' } });
 
 const VideoModel = mongoose.model('Video', videoSchema);
 
