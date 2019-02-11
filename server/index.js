608a2f24c402737ea10caff84f8390280b93f9f5
Katarina Svorcan <kata_che@hotmail.com>
2019-02-10 23:29:17 -0800

    set up jest unit tests

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/', (req, res) => { 
+  console.log(`Serving GET request on path: ${req.path}`)
+  res.sendFile(path.join(__dirname, '../public/index.html')) 
+})
+
+// catch-all
+app.get('*', (req, res) => { 
+  console.log(`Serving GET request on path: ${req.path}`)
+  res.send(404, 'This page does not exist! Please try another URL, or go back to home page!')
+})
+const server = app.listen(port, () => { console.log(`Example app listening on port ${port}!`) })
+
+module.exports = server;
