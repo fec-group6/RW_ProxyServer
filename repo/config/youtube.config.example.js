d9489a27dfacf3b64702971ac592d92634124ece
ronnywoods <rawoods93@gmail.com>
2019-02-19 05:37:27 -0800

    Add services as submodules
    See https://git-scm.com/book/en/v2/Git-Tools-Submodules

--- repo/config/youtube.config.example.js
+++
@@ -1,4 +0,0 @@
-// Put your YouTube API keys here!
-var YOUTUBE_API_KEY = 'YOUR KEY SHOULD GO HERE';
-
-module.exports = YOUTUBE_API_KEY;
--- repo/db/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/videoPlayerServiceDB';
-
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
-
-module.exports = db;
