61488d05bae6265d23dd04fcb77f9ddca5d62e11
ronnywoods <rawoods93@gmail.com>
2019-02-20 21:38:19 -0800

    Combine video and comments via proxy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'KS-V-P-Service/public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
